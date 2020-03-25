1b823e8474ae8df08d4ee7b48966301b1623babb
ingo <me@inba.us>
2020-03-25 13:39:22 +0100

    Update Node Color Detection
    Parameter:
    
    - x coordinate of scan window
    - y coordinate of scan window
    - width of scan window
    - height of scan window

--- LotusAlert/MainWindow.xaml.cs
+++ LotusAlert/MainWindow.xaml.cs
@@ -1,15 +1,14 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Diagnostics;
 using System.Drawing;
+using System.Drawing.Imaging;
 using System.IO;
-using System.Runtime.InteropServices;
+using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Media;
 using Color = System.Drawing.Color;
-using Point = System.Drawing.Point;
 
 namespace LotusAlert
 {
@@ -17,8 +16,9 @@ namespace LotusAlert
     {
         private CancellationTokenSource cancellationTokenSource;
         private readonly string CoordsFile = AppDomain.CurrentDomain.BaseDirectory + @"\coords.txt";
-        private readonly string ColorFile = AppDomain.CurrentDomain.BaseDirectory + @"\color.txt";
         private readonly string SoundFile = AppDomain.CurrentDomain.BaseDirectory + @"\gong.mp3";
+
+        private readonly string[] NodeColorArray = { "#E1B", "#E2B", "#E3B", "#E4B", "#E5B", "#E6B", "#E7B", "#E8B", "#E9B", "#EAB" };
 
         public MainWindow()
         {
@@ -28,50 +28,49 @@ namespace LotusAlert
 
         private void Init()
         {
-            TbxCoords.AcceptsReturn = true;
-            TbxCoords.TextWrapping = TextWrapping.Wrap;
-
             if (File.Exists(CoordsFile))
             {
                 TbxCoords.Text = File.ReadAllText(CoordsFile, System.Text.Encoding.UTF8);
-            }
-            if (File.Exists(ColorFile))
-            {
-                TbxColor.Text = File.ReadAllText(ColorFile, System.Text.Encoding.UTF8);
             }
         }
 
         private void BtnStart_Click(object sender, RoutedEventArgs e)
         {
-            Debug.WriteLine("Started.");
+            WriteLog("Started.");
             BtnStart.IsEnabled = false;
             BtnStop.IsEnabled = true;
             cancellationTokenSource = new CancellationTokenSource();
-            WatchLotusSpawn(TbxCoords.Text, TbxColor.Text);
+            WatchLotusSpawn(TbxCoords.Text);
         }
 
-        private void WatchLotusSpawn(string coords, string hexColor)
+        private void WatchLotusSpawn(string coords)
         {
+            var parts = coords.Split('/');
+            int xCoord = Convert.ToInt32(parts[0]);
+            WriteLog("X: " + xCoord);
+            int yCoord = Convert.ToInt32(parts[1]);
+            WriteLog("Y: " + yCoord);
+            int width = Convert.ToInt32(parts[2]);
+            WriteLog("Width: " + width);
+            int height = Convert.ToInt32(parts[3]);
+            WriteLog("Height: " + height);
+
             Task.Run(() =>
             {
                 while (true)
                 {
-                    Debug.WriteLine("Executing.");
-                    foreach (Point coord in GetCoords(coords))
+                    WriteLog("Searching for node ...");
+                    if (FindNode(xCoord, yCoord, width, height))
                     {
-                        if (CompareColor(coord, hexColor))
-                        {
-                            Debug.WriteLine("Found!");
-                            PlayGong();
-                            System.Windows.MessageBox.Show("At " + coord.X + "/" + coord.Y + " at " + DateTime.Now.ToString("HH:mm:ss"), "Lotus found!", MessageBoxButton.OK, MessageBoxImage.Information);
-                        }
+                        WriteLog("Node found.");
+                        PlayGong();
+                        MessageBox.Show("Node found at " + DateTime.Now.ToString("HH:mm:ss"), " Node found!", MessageBoxButton.OK, MessageBoxImage.Information);
                     }
-                    Thread.Sleep(5000);
-
-                    if (cancellationTokenSource.IsCancellationRequested)
+                    else
                     {
-                        throw new OperationCanceledException();
+                        WriteLog("No node found.");
                     }
+                    Thread.Sleep(1000);
                 }
             }, cancellationTokenSource.Token);
         }
@@ -86,76 +85,9 @@ namespace LotusAlert
             }
         }
 
-        private List<Point> GetCoords(string input)
-        {
-            var coords = new List<Point>();
-            var separator = new string[] { "\r\n" };
-
-            foreach (var line in input.Split(separator, StringSplitOptions.RemoveEmptyEntries))
-            {
-                try
-                {
-                    var rawCoords = line.Split('/');
-                    int inputX = Convert.ToInt32(rawCoords[0]);
-                    int inputY = Convert.ToInt32(rawCoords[1]);
-                    coords.Add(new Point(inputX, inputY));
-                }
-                catch (Exception)
-                {
-                    //Invalid coord, ignore
-                }
-            }
-
-            return coords;
-        }
-
-        private static bool CompareColor(Point coord, string hexColor)
-        {
-            try
-            {
-                if (!hexColor.StartsWith("#"))
-                {
-                    hexColor = "#" + hexColor;
-                }
-                Color colorInput = ColorTranslator.FromHtml(hexColor);
-                Color colorCoord = GetColorAt(coord.X, coord.Y);
-
-                if (colorInput.ToArgb() == colorCoord.ToArgb())
-                {
-                    return true;
-                }
-                return false;
-            }
-            catch (Exception)
-            {
-                return false;
-            }
-        }
-
-        [DllImport("user32.dll", SetLastError = true)]
-        public static extern IntPtr GetDesktopWindow();
-
-        [DllImport("user32.dll", SetLastError = true)]
-        public static extern IntPtr GetWindowDC(IntPtr window);
-
-        [DllImport("gdi32.dll", SetLastError = true)]
-        public static extern uint GetPixel(IntPtr dc, int x, int y);
-
-        [DllImport("user32.dll", SetLastError = true)]
-        public static extern int ReleaseDC(IntPtr window, IntPtr dc);
-
-        public static Color GetColorAt(int x, int y)
-        {
-            IntPtr desk = GetDesktopWindow();
-            IntPtr dc = GetWindowDC(desk);
-            int a = (int)GetPixel(dc, x, y);
-            ReleaseDC(desk, dc);
-            return Color.FromArgb(255, (a >> 0) & 0xff, (a >> 8) & 0xff, (a >> 16) & 0xff);
-        }
-
         private void BtnStop_Click(object sender, RoutedEventArgs e)
         {
-            Debug.WriteLine("Stop.");
+            WriteLog("Stop.");
             cancellationTokenSource.Cancel();
             BtnStart.IsEnabled = true;
             BtnStop.IsEnabled = false;
@@ -163,16 +95,48 @@ namespace LotusAlert
 
         private void BtnSave_Click(object sender, RoutedEventArgs e)
         {
-            if (File.Exists(CoordsFile))
-            {
-                File.Delete(CoordsFile);
-            }
-            if (File.Exists(ColorFile))
-            {
-                File.Delete(ColorFile);
-            }
             File.WriteAllText(CoordsFile, TbxCoords.Text);
-            File.WriteAllText(ColorFile, TbxColor.Text);
+        }
+
+        private bool FindNode(int x, int y, int width, int height)
+        {
+            Bitmap bitmap = new Bitmap(width, height);
+            using (Graphics g = Graphics.FromImage(bitmap))
+            {
+                g.CopyFromScreen(x, y, 0, 0, bitmap.Size);
+                //bitmap.Save(path, ImageFormat.Bmp);
+                //Bitmap savedBitmap = new Bitmap(path);
+                return FindColors(bitmap);
+            }
+        }
+
+        private bool FindColors(Bitmap bitmap)
+        {
+            for (int i = 0; i <= bitmap.Width - 1; i++)
+            {
+                for (int b = 0; b <= bitmap.Height - 1; b++)
+                {
+                    var foundColor = HexConverter(bitmap.GetPixel(i, b));
+                    if (NodeColorArray.Any(foundColor.StartsWith))
+                    {
+                        WriteLog("Found color (" + foundColor + ") at " + i + "/" + b);
+                        //bitmap.Save("z:\\node_spawn_" + foundColor + "_" + DateTime.Now.ToString("HHmmssfff") + ".bmp", ImageFormat.Bmp);
+                        return true;
+                    }
+                }
+            }
+            return false;
+        }
+
+        private static String HexConverter(Color color)
+        {
+            return "#" + color.R.ToString("X2") + color.G.ToString("X2") + color.B.ToString("X2");
+        }
+
+        private void WriteLog(string input)
+        {
+            var date = DateTime.Now.ToString("dd.MM.yy HH:mm:ss.fff");
+            Debug.WriteLine("[" + date + "] " + input);
         }
     }
 }
